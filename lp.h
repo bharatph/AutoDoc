c889da51212f5d8277de3550c5497a1a5604e380
Bharatvaj PH <bharatvaj@yahoo.com>
2017-09-30 00:19:54 +0530

    Retargeted project from AutoDoc Protocol(ADP) to Lazy Protocol(LP)

--- lp.h
+++ lp.h
@@ -1,5 +1,5 @@
 /*
- * adp.h
+ * lp.h
  *
  *  Created on: Jul 6, 2017
  */
@@ -35,8 +35,8 @@ Server replies with the following
 +---------+--------------------------------------------+
  *
  */
-#ifndef ADP_H
-#define ADP_H "ADP_H"
+#ifndef LP_H
+#define LP_H "LP_H"
 
 #include<iostream>
 #include<vector>
@@ -48,28 +48,10 @@ Server replies with the following
 #endif
 
 #include "clog.h"
-
-#define ADP_CON_REQ "CON_REQ"
-#define ADP_BIN_SEN "BIN_SEN"
-#define ADP_TXT_SEN "TXT_SEN"
-#define ADP_SEN_CMD "SEN_CMD"
-#define ADP_DIAG_ST "DIAG_ST"
-#define ADP_DIAG_CA "DIAG_CA"
-#define ADP_SET_BUF "SET_BUF"
-#define ADP_GET_BUF "GET_BUF"
-
-#define ADP_REQ_ACK "REQ_ACK"
-#define ADP_BINA_OK "BINA_OK"
-#define ADP_BINA_ER "BINA_ER"
-#define ADP_TEXT_OK "TEXT_OK"
-#define ADP_TEXT_ER "TEXT_ER"
-#define ADP_DIAG_OK "DIAG_OK"
-#define ADP_DIAG_ER "DIAG_ER"
-
-#define ADP_MSG_SIZE 7
-
-#define ADP_SERVER 0
-#define ADP_CLIENT 1
+#define LP_CON "CON"
+#define LP_ACK "ACK"
+#define LP_CMD "CMD"
+#define LP_BIN "BIN"
 
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 256
@@ -82,14 +64,14 @@ map<int, const char *> session_ids;
 void init_protocol(){
 	session_ids[0] = "__init__";
 }
-
-int adp_init_server(int _sockfd){
-	char *_str = (char *)malloc(ADP_MSG_SIZE);
-	if(read(_sockfd, _str, ADP_MSG_SIZE) == 7){
-			if(strncmp(_str, ADP_CON_REQ, ADP_MSG_SIZE) == 0){
-				log_inf(ADP_H, "Client found");
-				write(_sockfd, ADP_REQ_ACK, ADP_MSG_SIZE);
-				log_inf(ADP_H, "Client Acknowledged");
+/*
+int lp_init_server(int _sockfd){
+	char *_str = (char *)malloc(LP_MSG_SIZE);
+	if(read(_sockfd, _str, LP_MSG_SIZE) == 7){
+			if(strncmp(_str, LP_CON_REQ, LP_MSG_SIZE) == 0){
+				log_inf(LP_H, "Client found");
+				write(_sockfd, LP_REQ_ACK, LP_MSG_SIZE);
+				log_inf(LP_H, "Client Acknowledged");
 				return 0;
 			}
 			return -1;
@@ -97,24 +79,27 @@ int adp_init_server(int _sockfd){
 	return -1;
 }
 
-int adp_init_client(int _sockfd){
-		if(write(_sockfd, ADP_CON_REQ, ADP_MSG_SIZE) == 7){
-				char *_str = (char *)malloc(ADP_MSG_SIZE);
-				read(_sockfd, _str, ADP_MSG_SIZE);
-				if(strncmp(_str, ADP_REQ_ACK, ADP_MSG_SIZE) == 0){
-						log_inf(ADP_H, "Connection successful");
+int lp_init_client(int _sockfd){
+		if(write(_sockfd, LP_CON_REQ, LP_MSG_SIZE) == 7){
+				char *_str = (char *)malloc(LP_MSG_SIZE);
+				read(_sockfd, _str, LP_MSG_SIZE);
+				if(strncmp(_str, LP_REQ_ACK, LP_MSG_SIZE) == 0){
+						log_inf(LP_H, "Connection successful");
 						return 0;
 				}
-				log_inf(ADP_H, "Connection failed");
+				log_inf(LP_H, "Connection failed");
 				return -1;
 		}
 		return -1;
 }
 
+*/
 /*
  * same as write(int, void*, size_t) from <unistd.h>
  * but returns 0 or -1 indicating success or failure
  */
+
+/*
 int atomic_write(int _sockfd, const void *_msg, size_t _size){
 	static int _try;
 	if(_size == 0)return -1;//TEST _size <= 0 ?
@@ -134,17 +119,17 @@ int atomic_write(int _sockfd, const void *_msg, size_t _size){
 }
 
 //safe to call no-op
-int adp_send_text(int sockfd, const char *text){
+int lp_send_text(int sockfd, const char *text){
 	return -1;
 }
 
-int adp_send_binary(int sockfd, const char *binary){
+int lp_send_binary(int sockfd, const char *binary){
 	FILE *fp = fopen(binary, "rb");
 	if(fp){
-		log_err(ADP_H, "Cannot open the binary file");
+		log_err(LP_H, "Cannot open the binary file");
 		return -1;
 	}
-	atomic_write(sockfd, ADP_BIN_SEN, ADP_MSG_SIZE);
+	atomic_write(sockfd, LP_BIN_SEN, LP_MSG_SIZE);
 #if defined(__linux__)
 	sendfile(sockfd, fileno(fp), NULL, 1);
 #elif
@@ -157,7 +142,7 @@ int adp_send_binary(int sockfd, const char *binary){
 		}
 		else if(_bread < 0){
 			//handle errors
-			log_err(ADP_H, "Error occurred when reading file");
+			log_err(LP_H, "Error occurred when reading file");
 			return -1;
 		}
 		void *p = _buffer;
@@ -165,7 +150,7 @@ int adp_send_binary(int sockfd, const char *binary){
 			int _bwritten = write(sockfd, p, _bread);
 			if(_bwritten <=0){
 				//handle errors
-				log_err(ADP_H, "Error occurred while writing the file");
+				log_err(LP_H, "Error occurred while writing the file");
 				return -1;
 			}
 			_bread -= _bwritten;
@@ -175,5 +160,5 @@ int adp_send_binary(int sockfd, const char *binary){
 #endif
 	return 0;
 }
-
-#endif /* ADP_H */
+*/
+#endif /* LP_H */
