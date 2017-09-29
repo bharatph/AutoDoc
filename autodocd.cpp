108919b8deeecc7cea86558918f2418cedbe2267
Bharatvaj PH <bharatvaj@yahoo.com>
2017-09-30 00:27:36 +0530

    Added client handling functionalities and seperated code into the 'network_utils.h' file

--- autodocd.cpp
+++ autodocd.cpp
@@ -10,7 +10,7 @@
 #include "clog.h"
 #include "shell.h"
 #include "network_utils.h"
-#include"adp.h"
+#include"lp.h"
 
 int help(int, char **);
 
@@ -27,7 +27,7 @@ int send_details(int clifd, char **args){
 }
 
 int quit(int clifd, char **args){
-    write(clifd, "EXIT_ACK", BUFFER_SIZE);
+    writeln(clifd, "EXIT_ACK", BUFFER_SIZE);
     close(clifd);
     return 0;
 }
@@ -47,39 +47,13 @@ int help(int clifd, char **args){
 void *handle_new_connection(void * sock_desc){
     int clifd = *((int *)sock_desc);
     //AUTODOC PROTOCOL
-    adp_init_server(clifd);//handle error
-    char *buffer = (char *)malloc(BUFFER_SIZE);
+    //lp_init_server(clifd);//handle error
     int status = 0;
     while(status == 0){
-        read(clifd, buffer, BUFFER_SIZE);
+        char *buffer = readln(clifd);
         status = sh_process(jlen, jobs, buffer);
     }
     return NULL;
-}
-
-int initialize_server(int port){
-    //Create socket
-    servfd = socket(PF_INET , SOCK_STREAM , 0);
-    if (servfd == -1){
-        log_err(AUTODOC_D, "could not create socket");
-        return -1;
-    }
-    
-    struct sockaddr_in server;
-    //Prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( port );
-    //Bind
-    if( bind(servfd,(struct sockaddr *)&server , sizeof(server)) < 0){
-        log_err(AUTODOC_D, "bind failed");
-        return -1;
-    }
-    //Listen
-    listen(servfd , SERV_BACKLOG);
-    //Accept and incoming connection
-    log_inf(AUTODOC_D, "Waiting for incoming connections...");
-    return servfd;
 }
 
 void exit_handler(int sig){
@@ -91,7 +65,6 @@ int main(int argc, char *argv[]){
 	std::ios_base::sync_with_stdio(false);
     signal(SIGPIPE, SIG_IGN);
     signal(SIGINT, exit_handler);
-    init_clog(); //for mutex locks
     /*
 	pid_t pid;
 	pid = fork();
@@ -104,21 +77,11 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
      */
-    if((servfd = initialize_server(DEFAULT_PORT)) < 0){
-        log_fat(AUTODOC_D, "Cannot initialize server");
-        return -1;
-    }
-    log_inf(AUTODOC_D, "Server Initialized successfully");
-    int *new_sockfd;
-    struct sockaddr_in client;
-    socklen_t cli_size = sizeof(struct sockaddr_in);
-    //accept connection from an incoming client
     int clifd = -1;
-    while((clifd = accept(servfd, (struct sockaddr *)&client, &cli_size)) > 0){
+    while((clifd = start_server(DEFAULT_PORT)) > 0){
         log_inf(AUTODOC_D, "Connection accepted");
         //handle_new_connection(clifd);
-        new_sockfd = (int *)malloc(sizeof(int));
-        new_sockfd =  &clifd;
+        int *new_sockfd =  &clifd;
         pthread_create(&net_thread, NULL, &handle_new_connection, (void *)new_sockfd);
     }
     pthread_join(net_thread, NULL);
